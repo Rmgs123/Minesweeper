2afc1bd71a0e582dec9c2a71595b8a2f1bd5767e
Rmgs <Doctorfenichka@gmail.com>
2023-11-29 19:53:16 +1000

    Update 2
    Now we have classes and more functions!

--- main.py
+++ main.py
@@ -3,167 +3,154 @@
 import pygame
 import random
 
-# Game Constants
-WINDOW_SIZE = (800, 800)
-GRID_SIZE = (int(WINDOW_SIZE[0] * 0.75), int(WINDOW_SIZE[1] * 0.75))
-CELL_SIZE = GRID_SIZE[0] // 10  # Размер клетки, 10 клеток в ряд
+class MinesweeperGame:
+    def __init__(self):
+        self.window_size = (800, 800)
+        self.grid_size = (int(self.window_size[0] * 0.75), int(self.window_size[1] * 0.75))
+        self.cell_size = self.grid_size[0] // 10
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
+        self.RED = (255, 0, 0)
+        self.DARK_GREEN = (0, 100, 0)
+        self.GRAY = (150, 150, 150)
+        # Инициализация Pygame
+        pygame.init()
+        self.win = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption("Minesweeper")
 
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-DARK_GREEN = (0, 100, 0)
-GRAY = (150, 150, 150)
+        self.calculate_grid_position()
+        self.create_grid()
+        self.game_over = False  # Переменная для отслеживания конца игры
+        self.first_click = True  # Флаг первого клика
 
-# Initialize Pygame
-pygame.init()
-win = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption("Minesweeper")
+    def calculate_grid_position(self):
+        self.grid_top_left = ((self.window_size[0] - self.grid_size[0]) // 2, (self.window_size[1] - self.grid_size[1]) // 2)
 
-# Calculate the top-left corner of the grid to center it in the window
-GRID_TOP_LEFT = ((WINDOW_SIZE[0] - GRID_SIZE[0]) // 2, (WINDOW_SIZE[1] - GRID_SIZE[1]) // 2)
+    def create_grid(self):
+        self.rows = self.grid_size[1] // self.cell_size
+        self.cols = self.grid_size[0] // self.cell_size
+        self.mine_count = 20
+        self.grid = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+        self.revealed = [[False for _ in range(self.cols)] for _ in range(self.rows)]
+        self.flags = [[False for _ in range(self.cols)] for _ in range(self.rows)]
 
-# Create the grid
-ROWS = GRID_SIZE[1] // CELL_SIZE
-COLS = GRID_SIZE[0] // CELL_SIZE
-MINE_COUNT = 20
-grid = [[0 for _ in range(COLS)] for _ in range(ROWS)]
-revealed = [[False for _ in range(COLS)] for _ in range(ROWS)]
-flags = [[False for _ in range(COLS)] for _ in range(ROWS)]
+        self.generate_mines()
 
-# Function to generate the mines grid
-def generate_mines():
-    global grid, revealed, flags
-    grid = [[0 for _ in range(COLS)] for _ in range(ROWS)]
-    revealed = [[False for _ in range(COLS)] for _ in range(ROWS)]
-    flags = [[False for _ in range(COLS)] for _ in range(ROWS)]
+    def generate_mines(self):
+        mines = random.sample(range(self.rows * self.cols), self.mine_count)
+        for position in mines:
+            row = position // self.cols
+            col = position % self.cols
+            self.grid[row][col] = -1
 
-    mine_positions = random.sample(range(ROWS * COLS), MINE_COUNT)
-    for position in mine_positions:
-        row = position // COLS
-        col = position % COLS
-        grid[row][col] = -1
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.grid[row][col] != -1:
+                    count = sum(
+                        self.grid[i][j] == -1
+                        for i in range(max(0, row - 1), min(row + 2, self.rows))
+                        for j in range(max(0, col - 1), min(col + 2, self.cols))
+                    )
+                    self.grid[row][col] = count
 
-    for row in range(ROWS):
-        for col in range(COLS):
-            if grid[row][col] != -1:
-                count = 0
-                for i in range(max(0, row - 1), min(row + 2, ROWS)):
-                    for j in range(max(0, col - 1), min(col + 2, COLS)):
-                        if grid[i][j] == -1:
-                            count += 1
-                grid[row][col] = count
+    def reveal_adjacent_safe_cells(self, row, col):
+        if self.grid[row][col] == 0 and not self.revealed[row][col]:
+            self.revealed[row][col] = True
+            for i in range(max(0, row - 1), min(row + 2, self.rows)):
+                for j in range(max(0, col - 1), min(col + 2, self.cols)):
+                    self.reveal_adjacent_safe_cells(i, j)
+        elif self.grid[row][col] > 0:
+            self.revealed[row][col] = True
 
-# Function to reveal adjacent safe cells
-def reveal_adjacent_safe_cells(row, col):
-    for i in range(max(0, row - 1), min(row + 2, ROWS)):
-        for j in range(max(0, col - 1), min(col + 2, COLS)):
-            if not revealed[i][j]:
-                revealed[i][j] = True
-                if grid[i][j] == 0:
-                    reveal_adjacent_safe_cells(i, j)
+    def check_victory(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.grid[row][col] != -1 and not self.revealed[row][col]:
+                    return False
+                if self.grid[row][col] == -1 and not self.flags[row][col]:
+                    return False
+        return True
 
-# Function to check victory conditions
-def check_victory():
-    for row in range(ROWS):
-        for col in range(COLS):
-            if grid[row][col] != -1 and not revealed[row][col]:
-                return False
-            if grid[row][col] == -1 and not flags[row][col]:
-                return False
-    return True
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and not self.game_over:
+                if event.button == 1:  # Левая кнопка мыши
+                    x, y = event.pos
+                    row = (y - self.grid_top_left[1]) // self.cell_size
+                    col = (x - self.grid_top_left[0]) // self.cell_size
+                    if 0 <= row < self.rows and 0 <= col < self.cols:  # Проверка корректности координат
+                        if not self.flags[row][col]:
+                            if self.first_click:
+                                while self.grid[row][col] != 0 or self.grid[row][col] == -1:
+                                    self.create_grid()
+                                self.reveal_adjacent_safe_cells(row, col)
+                                self.first_click = False
+                            else:
+                                if self.grid[row][col] == -1:
+                                    self.revealed = [[True for _ in range(self.cols)] for _ in range(self.rows)]
+                                    self.game_over = True
+                                else:
+                                    if not self.revealed[row][col]:
+                                        self.revealed[row][col] = True
+                                        if self.grid[row][col] == 0:
+                                            self.reveal_adjacent_safe_cells(row, col)
+                elif event.button == 3:  # Правая кнопка мыши
+                    x, y = event.pos
+                    row = (y - self.grid_top_left[1]) // self.cell_size
+                    col = (x - self.grid_top_left[0]) // self.cell_size
+                    if 0 <= row < self.rows and 0 <= col < self.cols:  # Проверка корректности координат
+                        if not self.revealed[row][col]:
+                            self.flags[row][col] = not self.flags[row][col]
 
-# Main game loop
-running = True
-game_over = False
-first_click = True  # Flag to track the first click
-restart_rect = pygame.Rect(WINDOW_SIZE[0] // 2 - 50, 50, 100, 40)
-restart_font = pygame.font.Font(None, 36)
+    def draw_grid(self):
+        self.win.fill(self.WHITE)
+        for row in range(self.rows):
+            for col in range(self.cols):
+                x = self.grid_top_left[0] + col * self.cell_size
+                y = self.grid_top_left[1] + row * self.cell_size
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
-            if event.button == 1:  # Left mouse button
-                x, y = event.pos
-                row = (y - GRID_TOP_LEFT[1]) // CELL_SIZE
-                col = (x - GRID_TOP_LEFT[0]) // CELL_SIZE
-                if not flags[row][col]:  # Check if there is no flag
-                    if first_click:
-                        generate_mines()
-                        while grid[row][col] != 0:  # Keep regenerating until the first click is not zero
-                            generate_mines()
-                        reveal_adjacent_safe_cells(row, col)
-                        first_click = False
+                if self.revealed[row][col]:
+                    if self.grid[row][col] == -1:
+                        pygame.draw.rect(self.win, self.RED, (x, y, self.cell_size, self.cell_size))
+                    elif self.grid[row][col] == 0:
+                        pygame.draw.rect(self.win, self.DARK_GREEN, (x, y, self.cell_size, self.cell_size))
                     else:
-                        if grid[row][col] == -1:  # Lost by mine click
-                            revealed = [[True for _ in range(COLS)] for _ in range(ROWS)]
-                            game_over = True
-                        else:
-                            if not revealed[row][col]:
-                                revealed[row][col] = True
-                                if grid[row][col] == 0:
-                                    reveal_adjacent_safe_cells(row, col)
-            elif event.button == 3:  # Right mouse button
-                x, y = event.pos
-                row = (y - GRID_TOP_LEFT[1]) // CELL_SIZE
-                col = (x - GRID_TOP_LEFT[0]) // CELL_SIZE
-                if not revealed[row][col]:
-                    flags[row][col] = not flags[row][col]
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if restart_rect.collidepoint(event.pos):
-                first_click = True
-                game_over = False
-                generate_mines()
-
-    win.fill(WHITE)
-
-    # Draw the grid
-    for row in range(ROWS):
-        for col in range(COLS):
-            x = GRID_TOP_LEFT[0] + col * CELL_SIZE
-            y = GRID_TOP_LEFT[1] + row * CELL_SIZE
-
-            # Draw cell
-            if revealed[row][col]:
-                if grid[row][col] == -1:
-                    pygame.draw.rect(win, RED, (x, y, CELL_SIZE, CELL_SIZE))
-                elif grid[row][col] == 0:
-                    pygame.draw.rect(win, DARK_GREEN, (x, y, CELL_SIZE, CELL_SIZE))
+                        pygame.draw.rect(self.win, self.DARK_GREEN, (x, y, self.cell_size, self.cell_size))
+                        font = pygame.font.Font(None, self.cell_size // 2)
+                        text = font.render(str(self.grid[row][col]), True, self.BLACK)
+                        text_rect = text.get_rect(center=(x + self.cell_size // 2, y + self.cell_size // 2))
+                        self.win.blit(text, text_rect)
                 else:
-                    pygame.draw.rect(win, DARK_GREEN, (x, y, CELL_SIZE, CELL_SIZE))
-                    font = pygame.font.Font(None, CELL_SIZE // 2)
-                    text = font.render(str(grid[row][col]), True, BLACK)
-                    text_rect = text.get_rect(center=(x + CELL_SIZE // 2, y + CELL_SIZE // 2))
-                    win.blit(text, text_rect)
-            else:
-                pygame.draw.rect(win, GRAY, (x, y, CELL_SIZE, CELL_SIZE))
-                if flags[row][col]:
-                    pygame.draw.circle(win, RED, (x + CELL_SIZE // 2, y + CELL_SIZE // 2), CELL_SIZE // 4)
+                    pygame.draw.rect(self.win, self.GRAY, (x, y, self.cell_size, self.cell_size))
+                    if self.flags[row][col]:
+                        pygame.draw.circle(self.win, self.RED, (x + self.cell_size // 2, y + self.cell_size // 2), self.cell_size // 4)
 
-    # Draw Restart Button
-    pygame.draw.rect(win, GRAY, restart_rect)  # Remove white background
-    restart_text = restart_font.render("Restart", True, BLACK)
-    restart_rect_center = restart_text.get_rect(center=restart_rect.center)
-    win.blit(restart_text, restart_rect_center)
+        for i in range(self.cols + 1):
+            pygame.draw.line(self.win, self.BLACK, (self.grid_top_left[0] + i * self.cell_size, self.grid_top_left[1]),
+                             (self.grid_top_left[0] + i * self.cell_size, self.grid_top_left[1] + self.grid_size[1]))
+        for i in range(self.rows + 1):
+            pygame.draw.line(self.win, self.BLACK, (self.grid_top_left[0], self.grid_top_left[1] + i * self.cell_size),
+                             (self.grid_top_left[0] + self.grid_size[0], self.grid_top_left[1] + i * self.cell_size))
 
-    # Draw victory message
-    if not game_over and check_victory():
-        font = pygame.font.Font(None, 100)
-        text = font.render("You won!", True, (255, 225, 255))
-        text_rect = text.get_rect(center=(WINDOW_SIZE[0] // 2, GRID_TOP_LEFT[1] + GRID_SIZE[1] + 50))
-        win.blit(text, text_rect)
-        game_over = True
+    def run_game(self):
+        running = True
 
-    # Draw grid lines
-    for i in range(COLS + 1):
-        pygame.draw.line(win, BLACK, (GRID_TOP_LEFT[0] + i * CELL_SIZE, GRID_TOP_LEFT[1]),
-                         (GRID_TOP_LEFT[0] + i * CELL_SIZE, GRID_TOP_LEFT[1] + GRID_SIZE[1]))
-    for i in range(ROWS + 1):
-        pygame.draw.line(win, BLACK, (GRID_TOP_LEFT[0], GRID_TOP_LEFT[1] + i * CELL_SIZE),
-                         (GRID_TOP_LEFT[0] + GRID_SIZE[0], GRID_TOP_LEFT[1] + i * CELL_SIZE))
+        while running:
+            self.handle_events()
 
-    pygame.display.update()
+            if self.check_victory():
+                print("You won!")
+                self.game_over = True
 
-pygame.quit()
+            self.draw_grid()
+            pygame.display.update()
+
+            if self.game_over:
+                running = False
+
+if __name__ == "__main__":
+    minesweeper = MinesweeperGame()
+    minesweeper.run_game()
